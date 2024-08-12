1f22a38f0f6e41b1466a4bc0a1b8c153e1bacb8c
Emilio Campos <emencampos96@gmail.com>
2024-08-12 00:18:36 -0600

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import app from "./server";
 import config from "./config";
+import cors from "cors";
+
+app.use(cors());
 
 app.listen(config.port, () => {
   `Server listening at http://localhost:${config.port}`;
